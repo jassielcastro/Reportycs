7b3318578f9cb256ee33e8cd501ba5d17579c056
Alan Jassiel Castro Melgoza <alanme@backbase.com>
2024-11-14 17:48:21 -0600

    Add Dots component

---
+++ composeApp/src/commonMain/kotlin/ui/components/dots/ConnectedDotsScreen.kt
@@ -0,0 +1,67 @@
+package ui.components.dots
+
+import androidx.annotation.IntRange
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameMillis
+import androidx.compose.runtime.withFrameNanos
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.IntSize
+import kotlinx.coroutines.isActive
+
+@Composable
+fun ConnectedDotsScreen(
+    modifier: Modifier,
+    @IntRange(from = 1, to = 2)
+    dimension: Int,
+    @IntRange(from = 1L, to = 300L)
+    dotsSize: Int,
+    dotColors: List<Color>
+) {
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
+    var dots = remember { listOf<List<Dot>>() }
+    var framePosition by remember { mutableStateOf(0f) }
+
+    LaunchedEffect(Unit) {
+        while (isActive) {
+            withFrameMillis {
+                framePosition += 1f
+            }
+        }
+    }
+
+    Canvas(
+        modifier = modifier.onSizeChanged {
+            size = it
+            dots = DotMatrix.buildMatrix(
+                dimension = dimension,
+                numberOfDotsPerDimen = dotsSize,
+                drawIntoSize = size,
+                initialDotColor = dotColors
+            )
+        },
+        onDraw = {
+            if (size != IntSize.Zero && dots.isNotEmpty() && framePosition > 1f) {
+                dots.forEach { dotList ->
+                    dotList.forEach { startDot ->
+                        dotList.forEach { targetDot ->
+                            startDot.connect(this, targetDot)
+                        }
+                        startDot.moveInto(this.size)
+                        startDot.draw(this)
+                    }
+                }
+            }
+        },
+    )
+}
---
+++ composeApp/src/commonMain/kotlin/ui/components/dots/Dot.kt
@@ -0,0 +1,79 @@
+package ui.components.dots
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
+import kotlin.random.Random
+
+data class Dot(
+    var x: Float,
+    var y: Float,
+    val radio: Float = 5f,
+    var dirX: Float = Random.nextInt(-1, 1).toFloat(),
+    var dirY: Float = Random.nextInt(-1, 1).toFloat(),
+    val velocity: Float = 0.15f,
+    val color: Color
+) {
+    fun draw(scope: DrawScope) = with(scope) {
+        drawCircle(
+            color = color,
+            radius = radio,
+            center = Offset(x, y),
+            style = Stroke(width = 3.5f)
+        )
+    }
+
+    fun connect(scope: DrawScope, targetDot: Dot) = with(scope) {
+        if (targetDot == this@Dot) return@with
+
+        val dx = targetDot.x - x
+        val dy = targetDot.y - y
+
+        val distance = sqrt(dx.pow(2) + dy.pow(2))
+
+        if (distance < 80) {
+            val angle = atan2(dy, dx)
+
+            val startX = x + cos(angle) * radio
+            val startY = y + sin(angle) * radio
+
+            val endX = targetDot.x - cos(angle) * targetDot.radio
+            val endY = targetDot.y - sin(angle) * targetDot.radio
+
+            drawLine(
+                color = color,
+                start = Offset(startX, startY),
+                end = Offset(endX, endY),
+                strokeWidth = 2f
+            )
+        }
+    }
+
+    fun moveInto(size: Size) {
+        if (dirX == 0f) {
+            dirX = 1f
+        }
+
+        if (dirY == 0f) {
+            dirY = 1f
+        }
+
+        x += dirX * velocity
+        y += dirY * velocity
+
+        if (x + radio > size.width || x < 0) {
+            dirX *= -1
+        }
+
+        if (y + radio > size.height || y < 0) {
+            dirY *= -1
+        }
+    }
+}
